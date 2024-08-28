5d763f595f5d2c60828cacb0d822e58e2729e01b
DANNURA <nurudeendanjuma93@gmail.com>
2024-08-28 07:47:04 +0100

    update

--- fullstack/backend/server.js
+++ fullstack/backend/server.js
@@ -1,16 +1,18 @@
 const connect = require('./connect');
 const express = require('express');
 const cors = require('cors');
-//const posts = require('./postRoutes');
+const posts = require('./postRoutes');
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
-//app.use(posts);
+
+// Use '/posts' as the base path for all routes in postRoutes.js
+app.use('/posts', posts);
 
 app.listen(PORT, () => {
-    connect.connectToServer()
-    console.log('server is running on port ', PORT);
-})
+    connect.connectToServer();
+    console.log('Server is running on port', PORT);
+});
